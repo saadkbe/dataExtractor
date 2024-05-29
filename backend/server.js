9f39cb29d861ad76de1f55a33816481bb8b33a4e
saad kaabouche <saadkbe1@gmail.com>
2024-05-29 14:33:52 +0100

    Backend implentation

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,47 @@
 const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
-const { createWorker, worker } = require('tesseract.js');
+const { createWorker } = require('tesseract.js');
 
 const app = express();
 const port = 5000;
 
-// Allow requests from localhost:3000
 app.use(cors());
 
-// Multer configuration for handling file uploads
 const upload = multer({ dest: 'uploads/' });
 
-// Define a route to handle image uploads and text extraction
-
-app.post('/extract-text', upload.single('image'), async (req, res) => {
+app.post('/text', upload.single('image'), async (req, res) => {
   try {
-    console.log(req);
     if (!req.file) {
       return res.status(400).json({ error: 'No file uploaded' });
     }
 
-
-    const worker = createWorker({
-      logger: m => console.log(m) // Optional logger
-    });
+    const worker = await createWorker();
     await worker.load();
-    await worker.loadLanguage('eng+ara');
-    await worker.initialize('eng+ara');
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
     const { data } = await worker.recognize(req.file.path);
     await worker.terminate();
 
-    res.json({ text: data.text });
+    const extractedPrice = extractPrice(data.text);
+
+    res.json({ price: extractedPrice });
   } catch (error) {
     console.error('Error extracting text:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error });
   }
 });
+
+function extractPrice(text) {
+  const regex = /total\s*\:?\s*\$?(\d+(\.\d{1,2})?)/i;
+  const match = text.match(regex);
+  if (match) {
+    const price = match[1];
+    return parseFloat(price).toFixed(2);
+  } else {
+    return null;
+  }
+}
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
